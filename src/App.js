497ae3e2a4a1a0f3959dd321e88c85f33add3614
AndrewOhwoka <andrew.ohwoka@keyin.com>
2024-04-07 06:32:55 -0230

    April7First

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './img/logo.png';
 import './App.css';
 import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Products from './components/Products';
+import Details from './components/Details';
+import Payment from './components/Payment';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <React.Fragment>
+
+
+
+    <Navbar/>
+
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/details/:id" element={<Details />} />
+    </Routes>
+    
 
 
     </React.Fragment>
